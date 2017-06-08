35d33d5b2292d7569d90082aded91975e79fb7c5
soohyun SHAWN kim <imione9@gmail.com>
2017-06-08 10:37:02 +0900

    Add features
    Add commands
    - init [options]: initialize .env-switcher and apply to your project.
    - list: list your .env preset files
    - status: show you env status. at the moment, it supports showing which .env file is chosen.
    
    Change commands
    - $envswitch {env_name} —> $envswitch use {env_name}: As other commands added, add ‘use’ command
    to separate from other commands.

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
 const switcher = require('./lib');
 
-const PARAM_ERROR = 'no target parameters';
-
-function envSwitcher(params) {
-  if (params.length < 3) {
-    return console.log(PARAM_ERROR);
-  }
-
-  if (params[2] === 'init') { // init command
-    return switcher.initialize(params);
-  }
-
-  return switcher.switcher(params[2]);
-}
-
-exports.envSwitcher = envSwitcher;
+exports.envSwitcher = switcher;
